1dd2a3cd61c24cf70f5fa06a795980771e0ec892
MATHEUS BECKER <matheus_becker@estudante.sesisenai.org.br>
2023-07-10 16:50:51 -0300

    10/07

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import Unidades.Unidade;
+import Unidades.*;
 
 import java.util.Scanner;
 
@@ -36,11 +36,43 @@ public class Main {
         String cor = "";
         Unidade listaDeUnidades[] = new Unidade[3];
         String nome = "";
+        int decisao = 0;
         for (int i = 0; i < 2; i++) {
             System.out.println("Digite o nome do jogador " + (i + 1) + ":");
             nome = sc.next();
             if (i==0){
-                cor = "Branco"
+                cor = "Branco";
+            }else{
+                cor = "Preto";
+            }
+            System.out.println("escolha as unidades do jogador "+i+":"+"""
+                    1-Batman;
+                    2-Dragão;
+                    3-Hulk;
+                    4-Naruto;
+                    5-OptimusPrime
+                    """);
+
+            for (int j = 0; j <4 ; j++) {
+                decisao=sc.nextInt();
+                switch (decisao){
+                    case 1:
+                        Batman batman = new Batman(cor);
+                        listaDeUnidades[j]= batman;
+                        break;
+                    case 2:
+                        Dragao dragao = new Dragao(cor);
+                        break;
+                    case 3:
+                        Hulk hulk = new Hulk(cor);
+                        break;
+                    case 4:
+                        Naruto naruto = new Naruto(cor);
+                        break;
+                    case 5:
+                        OptimusPrime optimusPrime = new OptimusPrime(cor);
+                        break;
+                }
             }
 
         }
